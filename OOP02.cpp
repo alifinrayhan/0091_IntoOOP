7faffbc021a2d274ad4ffc8e0845f3edaac6d900
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-04-22 14:41:15 +0700

    class bangundatar

--- OOP02.cpp
+++ OOP02.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+class bangunDatar{
+    //akses modifier
+    private:
+        float panjang, lebar;
+    public:
+        float luas;
+
+
+        void input (){
+            cout << "Masukan Panjangnya = ";
+            cin >> panjang;
+            cout << "Masukan Lebarnya = ";
+            cin >> lebar;
+        }
+
+            
+}
